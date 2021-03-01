a5bdb096e2a79cc7a394d25e34c6cf2d11d89bd8
yuming.liu <yuming.liu@ihandysoft.com>
2021-03-01 22:53:13 +0800

    class

--- python_practice/test.py
+++ python_practice/test.py
@@ -2,39 +2,20 @@
 import contextlib
 import datetime
 import time
+import operator
 from urllib.parse import unquote
 from itertools import zip_longest
 
 # 测试用脚本
 
-print(unquote("http://52.83.208.161:8001/api/v1/event_stats?date_stop=20210207&event_name=%5B%5B%22__active%22%5D%5D&date_start=20201209&from_cache=True&timeout=30&result_id=audience-7etjhsc3d"))
+print(unquote("http://52.83.208.161:8001/api/v1/event_stats?date_stop=20210219&event_name=%5B%22__user_earnings_rmb%22%2C+%22__active%22%5D&date_start=20210219&from_cache=True&timeout=30&result_id=case-7two1n7fj"))
 
 
-class Father(object):
+def student(*args, male, graduated):
 
-    color = "red"
+    [print(a) for a in args]
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    print("male: {}".format(male))
+    print("graduated: {}".format(graduated))
 
-    def scold(self):
-        print("$%#^&$#@$!$@")
-
-
-class Son(Father):
-
-    def __init__(self, height, name, age):
-        self.height = height
-        super(Son, self).__init__(name, age)
-
-    def cry(self):
-        print("哇哇哇哇")
-
-
-a = {"a": 1, "b": 2, "c": 3, "d": 4}
-for k , v in a.items():
-    if k == "b":
-        a.pop(k)
-    else:
-        print(k)
+student("Max","lala", male=True, graduated=False)
