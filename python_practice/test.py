3a1bf2bfec8b0bdedbbdef29589176a4e9285b01
yuming.liu <yuming.liu@ihandysoft.com>
2021-03-15 19:12:58 +0800

    1

--- python_practice/test.py
+++ python_practice/test.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 import contextlib
 import datetime
+import collections
 import time
 import operator
 from urllib.parse import unquote
 from itertools import zip_longest
+import re
 
-# 测试用脚本
+url = unquote("http://test-ap-dataservice.appcloudbox.net:8081/api/v2/case_segment_analysis?report_id=6&metric=ltv_7&dimensions=['device_brand']")
+print(url)
 
-print(unquote("http://52.83.208.161:8001/api/v1/event_stats?date_stop=20210219&event_name=%5B%22__user_earnings_rmb%22%2C+%22__active%22%5D&date_start=20210219&from_cache=True&timeout=30&result_id=case-7two1n7fj"))
+
+class A(object):
+
+    def __init__(self):
+        self.a = 10
+        self.b = 20
 
 
-def student(*args, male, graduated):
+a = A()
+print(hasattr(a, "c"))
 
-    [print(a) for a in args]
-
-    print("male: {}".format(male))
-    print("graduated: {}".format(graduated))
-
-student("Max","lala", male=True, graduated=False)
